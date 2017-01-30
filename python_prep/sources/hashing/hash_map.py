b0e8908116abb4bf3a3edee7ca9487116a124404
ColinConduff <cconduff098@gmail.com>
2017-01-30 11:52:12 -0600

    Implement and test deletion and contraction of container.

--- python_prep/sources/hashing/hash_map.py
+++ python_prep/sources/hashing/hash_map.py
@@ -5,28 +5,31 @@ class HashMap(object):
 
 	Currently uses linear probing.
 	"""
+	_DELETION_FLAG = '\n' # replace with enum?
 
 	def __init__(self, items=None):
 		self._item_count = 0
-		self._container_size = 8
 
 		if items is None:
+			self._container_size = 8
 			self._keys = [None for _ in range(self._container_size)]
 			self._values = [None for _ in range(self._container_size)]
 		else:
+			# Avoid expansion operations
+			self._container_size = self._next_power_of_2(len(items))
 			self._new_keys_and_values(items)
 
 	@property 
 	def keys(self):
-		return iter(x for x in self._keys if x is not None)
+		return iter(x for x in self._keys if self._is_valid_item(x))
 
 	@property 
 	def values(self):
-		return iter(x for x in self._values if x is not None)
+		return iter(x for x in self._values if self._is_valid_item(x))
 
 	@property 
 	def items(self):
-		return iter(x for x in zip(self._keys, self._values) if x[0] is not None)
+		return iter(x for x in zip(self._keys, self._values) if self._is_valid_item(x[0]))
 
 	def __iter__(self):
 		return self.items
@@ -35,29 +38,92 @@ class HashMap(object):
 		return self._item_count
 
 	def __contains__(self, key):
-		return self._index_of(key) is not None
+		return self._search(key)
 
 	def __getitem__(self, key):
-		index = self._index_of(key)
-		return self._values[index] if index is not None else None
+		index = self._search(key)
+		if index is not None:
+			return self._values[index] 
+		else:
+			raise KeyError
 
 	def __setitem__(self, key, value):
 		self._insert_item(key, value)
 
-	def _index_of(self, key):
+	def __delitem__(self, key):
+		""" 
+		Replace the key's slot with a special flag indicating deletion. 
+
+		Raises: 
+			KeyError if the key is not in the hash map.
+		"""
+		self._delete_item(key)
+
+	def _is_valid_item(self, item):
+		return item is not None and item != HashMap._DELETION_FLAG
+
+	def _search(self, key):
+		""" 
+		Return the index corresponding to the key.
+
+		Amortized O(1) 
+		"""
+
 		for index in self._find_index(key):
+			# skips special flag indicating deletion
 			if self._keys[index] is None:
 				return None
 			if self._keys[index] == key:
 				return index
 
+	def _insert_item(self, key, value):
+		""" Amortized Theta(1) """
+		self._expand_container_if_necessary()
+
+		for index in self._find_index(key):
+			if self._keys[index] is None or self._keys[index] == HashMap._DELETION_FLAG:
+				self._item_count += 1
+				self._keys[index] = key
+				self._values[index] = value
+				break
+
+	def _delete_item(self, key):
+		""" Amortized O(1) """
+		self._contract_container_if_necessary()
+
+		for index in self._find_index(key):
+			if self._keys[index] is None:
+				raise KeyError
+
+			if self._keys[index] == key:
+				self._item_count -= 1
+				self._keys[index] = HashMap._DELETION_FLAG
+				self._values[index] = None
+				break
+
+	def _expand_container_if_necessary(self):
+		""" Amortized Theta(1) """
+		if self._item_count + 1 >= self._container_size:
+			self._container_size *= 2
+			self._new_keys_and_values(list(self.items))
+
+	def _contract_container_if_necessary(self):
+		""" Amortized O(1) """
+		if self._item_count <= self._container_size / 4:
+			self._container_size //= 2
+			self._new_keys_and_values(list(self.items))
+
 	def _new_keys_and_values(self, items):
 		""" 
+		Create new containers and insert the previous items into it.
+
 		Best-case:
-			No collisions 
+			Expansions not needed, no collisions occur
 			Theta(n)
 		Worst-case:
 			Collision occurs on every insertion
+			e.g. insert [0, 8, 16, 32 ...] 
+			result of using linear probing
 			Theta(n^2)
 		"""
 		self._item_count = 0
@@ -67,29 +133,6 @@ class HashMap(object):
 
 		for key, value in items:
 			self._insert_item(key, value)
-
-	def _insert_item(self, key, value):
-		""" Amortized O(1) """
-		self._expand_container_if_necessary()
-
-		for index in self._find_index(key):
-			if self._keys[index] is None:
-				self._item_count += 1
-				self._keys[index] = key
-				self._values[index] = value
-				break
-
-	def _expand_container_if_necessary(self):
-		""" Amortized O(1) """
-		if self._item_count + 1 >= self._container_size:
-			self._container_size *= 2
-			self._new_keys_and_values(list(self.items))
-
-	def _contract_container_if_necessary(self):
-		""" Amortized O(1) """
-		if self._item_count <= self._container_size / 4:
-			self._container_size *= 2
-			self._new_keys_and_values(list(self.items))
 
 	def _find_index(self, key):
 		""" 
@@ -103,23 +146,29 @@ class HashMap(object):
 		Worst-case:
 			Collision occurs and all subsequent slots are full, except last one
 			Theta(n)
-
-		Using the following iter might be more straightforward 
-		iter(list(range(hash_index, _container_size)) + list(range(hash_index)))
 		"""
-		count = iter(range(self._container_size))
-		index = hash(key) % self._container_size
+		hash_index = hash(key) % self._container_size
 		
+		left_half = list(range(hash_index))
+		right_half = list(range(hash_index, self._container_size))
+		
+		indices = iter(right_half + left_half)
+
 		while True:
-			yield index
-			
-			index += 1
-			index %= self._container_size
-			
 			try:
-				next(count)
+				yield next(indices)
 			except StopIteration:
 				break
+
+	def _next_power_of_2(self, n):
+		n -= 1
+		n |= n >> 1
+		n |= n >> 2
+		n |= n >> 4
+		n |= n >> 8
+		n |= n >> 16
+		n += 1
+		return n
 
 
 
--- python_prep/tests/test_hashing.py
+++ python_prep/tests/test_hashing.py
@@ -6,22 +6,46 @@ from sources.hashing.hash_map import HashMap
 class HashingTestCase(unittest.TestCase):
 
 	def test_hash_map(self):
-		keys = [1,2,3,4,5,6,7,8,9]
+		keys = list(range(11, 100))
 		items = list(zip(keys, keys))
 		hash_map = HashMap(items)
 		
-		self.assertTrue(len(hash_map) == 9)
+		# Test len
+		self.assertTrue(len(hash_map) == len(keys))
+
+		# Test keys
 		self.assertTrue(set(hash_map.keys) == set(keys))
+
+		# Test values
 		self.assertTrue(set(hash_map.values) == set(keys))
+
+		# Test items
 		self.assertTrue(set(hash_map.items) == set(items))
 
+		# Test contains
 		self.assertTrue(10 not in hash_map)
-		hash_map[10] = 10
-		self.assertTrue(len(hash_map) == 10)
 
+		# Test __setitem__
+		hash_map[10] = 10
+		self.assertTrue(len(hash_map) == len(keys) + 1)
+
+		# Test iter, contains, and __getitem__
 		for key, value in hash_map:
 			self.assertTrue(key in hash_map)
 			self.assertTrue(hash_map[key] == value)
+
+		# Test __delitem__
+		del hash_map[11]
+		self.assertTrue(len(hash_map) == len(keys))
+		self.assertTrue(11 not in hash_map)
+
+		# Test insertion after deletion
+		hash_map[1] = 1 
+		self.assertTrue(len(hash_map) == len(keys) + 1)
+
+		# Test contraction
+		for key in range(12, 100):
+			del hash_map[key]
 
 
 if __name__ == '__main__':
